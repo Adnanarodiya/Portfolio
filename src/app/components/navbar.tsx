5e0c7873fd2472b8777db30b13d81dab7eb98d9d
Adnan Arodiya <aiarodiya07@gmail.com>
2024-02-28 22:01:10 +0530

    Add dropdown menu to navbar and update styling in page component to responsive

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,18 +1,41 @@
 import { Caveat } from "next/font/google";
 import React from "react";
 import { cn } from "@/utils/cn";
+import { Menu } from "lucide-react";
 const caveat = Caveat({ subsets: ["latin"] });
 
 export default function Navbar() {
   return (
-    <div className="sticky top-5">
+    <div className="sticky top-5 z-50">
       <div className="navbar bg-base-100 rounded-3xl w-11/12 mx-auto">
         <div className="flex-1">
           <a className={cn("btn btn-ghost text-xl", caveat.className)}>
             Adnan Arodiya
           </a>
         </div>
-        <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn">
+            <Menu />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>Skills</a>
+            </li>
+            <li>
+              <a>Resume</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-none hidden">
           <ul className="menu menu-horizontal px-1">
             <li>
               <a>Home</a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="w-11/12 p-8  flex mx-auto h-screen flex-col justify-center">
+      <div className=" w-11/12 p-8  flex mx-auto h-screen flex-col justify-center">
         <div>
-          <h3 className="text-2xl font-semibold ">
+          <h3 className=" md:text-2xl font-semibold ">
             Hello, My name is Adnan Arodiya .
           </h3>
-          <h1 className="text-6xl font-bold mt-8">I build frontend stuffs.</h1>
-          <h4 className="mt-8 text-2xl">
+          <h1 className="text-4xl md:text-6xl font-bold mt-4 md:mt-8">
+            I build frontend stuffs.
+          </h1>
+          <h4 className=" mt-4 md:mt-8 md:text-2xl">
             I'm a frontend developer, who loves to craft things with frontend
             skills, based in India/Gujarat.
           </h4>
@@ -26,18 +28,16 @@ export default function Home() {
       </div>
       <div className="w-11/12 px-8 py-4 mx-auto flex items-center h-[calc(100vh-10rem)] flex-col">
         <h1 className="text-3xl font-bold">About me</h1>
-        <div className="flex mt-10 justify-evenly">
-          <div className="w-2/4 flex flex-col justify-center items-center text-xl italic">
-            <div className="flex">
-              <div>
+        <div className="flex mt-10 justify-evenly flex-col md:flex-row">
+          <div className=" md:w-2/4 flex flex-col justify-center items-center text-xl italic order-last">
+            <div className="flex text-sm mt-8 md:mt-0 text-center">
+              {/* <div>
                 <ChevronsRight />
-              </div>
+              </div> */}
               Hi! I'm Adnan , a passionate front-end developer with a solid
               skill set in Next.js and React.js. I thrive on creating
               captivating user interfaces and am on a constant quest to learn
-              and innovate. Currently exploring full-stack development, I bring
-              creativity and precision to every project. Let's craft something
-              extraordinary together!
+              and innovate. Currently exploring full-stack development.
             </div>
           </div>
           <div>
@@ -51,41 +51,42 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-11/12 px-8 py-4 mx-auto flex items-center h-[calc(100vh-10rem)] flex-col relative mt-14">
-        <h2 className="text-4xl font-bold mb-8">Timeline</h2>
+      <div className="md:w-11/12 px-8 py-4 md:mx-auto flex md:items-center h-[calc(100vh-10rem)] flex-col relative mt-14 z-10">
+        <h2 className="text-center mt-10 text-4xl font-bold mb-8">Timeline</h2>
         <div className="relative ">
-          <div className="border-4 border-white absolute rounded-3xl left-2/4 -translate-x-2/4 h-96 right-2"></div>
-          <div className="absolute p-4 bg-base-200 rounded-full left-2/4 -translate-x-2/4">
+          <div className="border-4 border-white absolute rounded-3xl md:left-2/4 md:-translate-x-2/4 h-[660px] md:h-96 left-6 md:right-2"></div>
+          <div className="absolute p-4 bg-base-200 rounded-full md:left-2/4 md:-translate-x-2/4 md:right-2">
             <GraduationCap />
           </div>
-          <div className="absolute p-4 bg-base-200 rounded-full top-48 left-2/4 -translate-x-2/4">
+          <div className="absolute p-4 bg-base-200 rounded-full top-52 md:top-48 md:left-2/4 md:-translate-x-2/4 md:right-2">
             <GraduationCap />
           </div>
-          <div className="absolute p-4 bg-base-200 rounded-full top-[384px] left-2/4 -translate-x-2/4">
+          <div className="absolute p-4 bg-base-200 rounded-full top-[450px] md:top-[384px] md:left-2/4 md:-translate-x-2/4 md:right-2">
             <GraduationCap />
           </div>
         </div>
-        <div className="card rounded-tl-3xl rounded-tr-none rounded-bl-none rounded-br-rounded-tr-3xl  w-64 bg-base-200  right-44 top-8">
+        <div className="card rounded-tr-3xl rounded-tl-none rounded-br-none rounded-bl-rounded-tr-3xl md:rounded-tl-3xl md:rounded-tr-none md:rounded-bl-none md:rounded-br-rounded-tr-3xl  w-52 left-16 md:w-64 bg-base-200  md:right-44 top-14 md:top-8">
           <div className="card-body">
-            <h2 className="card-title">High School</h2>
-            <p>2019 - 2021</p>
+            <h2 className=" md:card-title text-base font-bold">High School</h2>
+            <p className="text-sm md:text-base">2019 - 2021</p>
           </div>
         </div>
-        <div className="card rounded-tr-3xl rounded-tl-none rounded-br-none rounded-bl-rounded-tr-3xl  w-64 bg-base-200  left-44 top-24">
+        <div className="card rounded-tr-3xl rounded-tl-none rounded-br-none rounded-bl-rounded-tr-3xl   w-52 left-16 md:w-64 bg-base-200  md:right-44 top-36 md:top-24">
           <div className="card-body">
-            <h2 className="card-title  ">Uka Tarsadia University (CGPIT)</h2>
-            <p>2021 - 2024</p>
+            <h2 className="md:card-title text-base font-bold  ">
+              Uka Tarsadia University (CGPIT)
+            </h2>
+            <p className="text-sm md:text-base">2021 - 2024</p>
           </div>
         </div>
-        <div className="card rounded-tl-3xl rounded-tr-none rounded-bl-none rounded-br-rounded-tr-3xl  w-64 bg-base-200  right-44 top-36">
+        <div className="card rounded-tr-3xl rounded-tl-none rounded-br-none rounded-bl-rounded-tr-3xl md:rounded-tl-3xl md:rounded-tr-none md:rounded-bl-none md:rounded-br-rounded-tr-3xl  w-52 left-16 md:w-64 bg-base-200  md:right-44 top-56 md:top-36">
           <div className="card-body">
-            <h2 className="card-title">Internship at</h2>
-            <p>EnactOn Technologies Private Limited</p>
+            <h2 className="md:card-title text-base font-bold">Internship at</h2>
+            <p className="text-sm md:text-base">
+              EnactOn Technologies Private Limited
+            </p>
           </div>
         </div>
-      </div>
-      <div className="w-11/12 px-8 py-4 mx-auto flex items-center h-[calc(100vh-10rem)] flex-col">
-        get in touch
       </div>
     </div>
   );
